976db370cf4351baa2096bf5b1e82c2ef3f59b97
Dmitriy Blyndaruk <dmitriy.blyndaruk@gmail.com>
2021-03-31 02:19:18 +0300

    og image/meta seo tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,70 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'portfolio',
+    title: 'NDark Agency',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'NDark Agency is a web & mobile app development company. ' +
+          'We build for the Web with HTML/CSS, JavaScript, PHP and Agile, ' +
+          'providing UI/UX design and convenient website admin features. ' +
+          'We work closely with you to design ' +
+          'and build your digital products. Request a proposal!',
+      },
+      {
+        hid: 'og:description',
+        name: 'og:description',
+        content:
+          'NDark Agency is a web & mobile app development company. ' +
+          'We build for the Web with HTML/CSS, JavaScript, PHP and Agile, ' +
+          'providing UI/UX design and convenient website admin features. ' +
+          'We work closely with you to design ' +
+          'and build your digital products. Request a proposal!',
+      },
+      {
+        hid: 'og:site_name',
+        name: 'og:site_name',
+        content: 'NDark Agency',
+      },
+      {
+        hid: 'apple-mobile-web-app-title',
+        name: 'apple-mobile-web-app-title',
+        content: 'NDark Agency',
+      },
+      {
+        hid: 'og:title',
+        name: 'og:title',
+        content: 'NDark Agency - We develop and design Web & mobile apps',
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: `${process.env.PUBLIC_SITE_URL}/ndark-og.png`,
+      },
+      {
+        hid: 'twitter:card',
+        property: 'twitter:card',
+        content: 'summary_large_image',
+      },
+      {
+        hid: 'twitter:image',
+        property: 'twitter:image',
+        content: `${process.env.PUBLIC_SITE_URL}/ndark-og.png`,
+      },
+      {
+        rel: 'preconnect',
+        href: process.env.PUBLIC_SITE_URL,
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  env: {
+    BASE_URL: process.env.baseUrl || 'http://localhost:3000',
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
